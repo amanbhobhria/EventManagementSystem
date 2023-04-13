a7d0450942dc2716f09c2ee5749809906cefb22a
amanbhobhria <75074918+amanbhobhria@users.noreply.github.com>
2023-04-13 23:33:48 +0530

    undo
    sjn

--- app/src/main/java/com/example/eventmanagementsystem/HomeActivity.kt
+++ app/src/main/java/com/example/eventmanagementsystem/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.eventmanagementsystem
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
@@ -12,11 +13,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eventmanagementsystem.adapter.EventsAdapter
 import com.example.eventmanagementsystem.model.EventsModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.database.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
     private val ChannelId="New Event Channel"
     private val NotificationId= 123
+
     var progressBar: ProgressBar? = null
 
 
@@ -24,6 +30,15 @@ class HomeActivity : AppCompatActivity() {
     var reference: DatabaseReference? = null
     var recyclerView: RecyclerView? = null
     lateinit var addNewBtn: Button
+
+    var bottomNavigationView: BottomNavigationView? = null
+
+
+    private val recent = "recent"
+    private val upcoming = "upcoming"
+    private val completed = "completed"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -34,8 +49,9 @@ class HomeActivity : AppCompatActivity() {
 
         initialize()
 
-        getEventsData()
+//        getEventsData()
         addNewEvent()
+        bottomNavigation()
         recyclerView!!.layoutManager = LinearLayoutManager(this)
 
 
@@ -49,6 +65,7 @@ class HomeActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.showOnGoingRecyclerView)
 
         progressBar = findViewById(R.id.loadingProgressBar)
+        bottomNavigationView = findViewById(R.id.bottom_nav)
 
 
     }
@@ -95,6 +112,79 @@ class HomeActivity : AppCompatActivity() {
 
 
     }
+
+
+    private fun showBookings(status: String) {
+        val list: MutableList<EventsModel> = ArrayList<EventsModel>()
+        reference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                list.clear()
+
+                for (sp in snapshot.children) {
+                    val eventModel: EventsModel = sp.getValue(EventsModel::class.java)!!
+
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                    val d = Calendar.getInstance()
+                    when (status) {
+
+
+
+                        upcoming -> if (eventModel.eventDate>sdf.format(d.time))
+                         {
+                            eventModel.setEventId(sp.key)
+                            list.add(eventModel)
+                        }
+                        recent -> if (eventModel.eventDate==sdf.format(d.time)
+                        ) {
+
+                            eventModel.setEventId(sp.key)
+                            list.add(eventModel)
+                        }
+                        completed -> if (eventModel.eventDate<sdf.format(d.time)
+
+                        ) {
+
+                            eventModel.setEventId(sp.key)
+                            list.add(eventModel)
+                        }
+                    }
+                }
+                val eventsAdapter = EventsAdapter(list, this@HomeActivity)
+                recyclerView!!.adapter = eventsAdapter
+
+                    recyclerView!!.visibility = View.VISIBLE
+                }
+
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@HomeActivity, error.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
+    private fun bottomNavigation() {
+        bottomNavigationView?.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item: MenuItem ->
+            if (item.itemId == R.id.bottom_nav_upcoming) {
+                try {
+                    showBookings(upcoming)
+
+                } catch (e: Exception) {
+                    Toast.makeText(this@HomeActivity, e.toString(), Toast.LENGTH_SHORT).show()
+                }
+                item.isChecked = true
+            } else if (item.itemId == R.id.bottom_nav_recent) {
+                showBookings(recent)
+                item.isChecked = true
+            } else if (item.itemId == R.id.bottom_nav_completed) {
+                showBookings(completed)
+                item.isChecked = true
+            }
+            false
+        })
+    }
+
+
 }
 
 
